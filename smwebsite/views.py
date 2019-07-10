163b78f62a4ed778b517bf88f5a51a11e1bbc56f
Alejandro Lugo <elo.lugo@gmail.com>
2019-07-10 18:09:57 -0400

    Added dissemination page w/o working links

--- smwebsite/views.py
+++ smwebsite/views.py
@@ -42,8 +42,17 @@ class BlogHomePageView(TemplateView):
         return super().dispatch(request, *args, **kwargs)
 
 
+class DisseminationHomePageView(TemplateView):
+    template_name = "dissemination/index.html"
+
+    def dispatch(self, request, *args, **kwargs):
+        translation.activate(request.LANGUAGE_CODE)
+        return super().dispatch(request, *args, **kwargs)
+
+
 home = HomePageView.as_view()
 ucarpooling = UcarpoolingHomePageView.as_view()
 smartparking = SmartparkingHomePageView.as_view()
 smartmoving = SmartmovingHomePageView.as_view()
 blog = BlogHomePageView.as_view()
+dissemination = DisseminationHomePageView.as_view()
